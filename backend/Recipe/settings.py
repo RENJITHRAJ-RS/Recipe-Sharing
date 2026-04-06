9de6df31a54b9229b8f8da05810be7de70d78515
RENJITHRAJ-RS <renjithraj911@gmail.com.com>
2026-04-06 21:26:44 +0530

    changed mysql to sqlite for render

--- backend/Recipe/settings.py
+++ backend/Recipe/settings.py
@@ -82,16 +82,8 @@ WSGI_APPLICATION = 'Recipe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'recipelisting',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
